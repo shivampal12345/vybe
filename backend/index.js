30464025179453cfeb64181f9eabde68dfd805a2
Shivam Pal <samratpal1997@gmail.com>
2025-11-16 19:00:01 +0530

    Change CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://vybe-hc1y.onrender.com",
     credentials:true
 }))
 app.use(express.json())
